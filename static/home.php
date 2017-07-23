2753299aea75b573f5ffdcf663228feddf6035f9
Fanny Hasbi <fnyhsbi@gmail.com>
2017-07-23 21:26:51 +0700

    Edit home with Materialize framework

--- static/home.php
+++ static/home.php
@@ -3,10 +3,29 @@
 <head>
   <title>Voter Seed</title>
   <meta charset="utf-8">
+
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="<?= _URL ?>static/assets/css/materialize.min.css">
+
+  <style>
+    html, body, header {
+      height: 100%;
+      width: 100%;
+    }
+  </style>
 </head>
 <body>
-<?= _BASEURL ?>
-<h1>Hello from Voter Seed!</h1>
+  <header class="valign-wrapper">
+    <div class="row">
+      <div class="col l12">
+        <h1 class="center-align">Voter Seed</h1>
+        <a href="#" class="waves-effect waves-light btn"><i class="material-icons left">library_books</i>VOTING</a>
+        <a href="#" class="waves-effect waves-light btn"><i class="material-icons left">library_books</i>PANDUAN</a>
+      </div>
+    </div>
+  </header>
 
+  <script src="<?= _URL.'static/assets/js/jquery-3.2.1.min.js'?>"></script>
+  <script src="<?= _URL.'static/assets/js/materialize.min.js'?>"></script>
 </body>
 </html>
